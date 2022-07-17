c7c322cd8b567b5a7897c166936e4b4e530d0211
Nana Pisanko <bonjozzy@gmail.com>
2022-07-18 00:26:45 +0300

    feat: add close btn open style

--- js/index.js
+++ js/index.js
@@ -16,9 +16,18 @@ plantsBlock.addEventListener('click', function openArticle(e){
 /* ---------------------------- BURGER MENU ---------------- */
 
 const closeBtn = document.querySelector('.close-lines');
-
+const line1 = document.querySelector('.close-lines__line1');
+const line2 = document.querySelector('.close-lines__line2');
+const line3 = document.querySelector('.close-lines__line3');
 
 closeBtn.addEventListener('click', () => {
 	console.log('click close!');
 	document.querySelector('.nav').classList.toggle('show-menu');
-})
+	line1.classList.toggle('close-lines__line1-opened');
+	line2.classList.toggle('close-lines__line2-opened');
+	line3.classList.toggle('close-lines__line3-opened');
+});
+
+
+
+
